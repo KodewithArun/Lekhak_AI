ba31f8cf562b95fdf5a3792c1875f16fcd5b3ba9
KodewihArun <contactwitharunlaudari@gmail.com>
2025-11-26 20:42:29 +0545

    deleted agent_runner.py as we make the seprate modular function in service

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,7 +2,6 @@
 Utility functions for Lekhak AI
 """
 
-from src.utils.agent_runner import call_agent_async
 from src.utils.loggers import get_logger
 
-__all__ = ["call_agent_async", "get_logger"]
+__all__ = ["get_logger"]
